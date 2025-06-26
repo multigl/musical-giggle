183412eca84ca616fe921c91588e09294ee1728c
Joey Leingang-Hochberg <multigl@gmail.com>
2025-06-26 11:10:43 +0000

    add zsh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,6 +10,6 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y vim tuned tuned-ppd tuned-profiles-atomic
+dnf5 install -y zsh vim tuned tuned-ppd tuned-profiles-atomic
 
 echo "export EDITOR=vim" > /etc/profile.d/zz-default-editor.sh
