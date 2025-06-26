fa890ba3b5326d7234855f0b81aee2fc45ac3ad0
Joey Leingang-Hochberg <multigl@gmail.com>
2025-06-26 11:08:57 +0000

    cosign pubkey, base off of ucore

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,6 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y vim tuned tuned-ppd tuned-profiles-atomic
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+echo "export EDITOR=vim" > /etc/profile.d/zz-default-editor.sh
